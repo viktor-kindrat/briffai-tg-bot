4642e6708d8c6cd2546fb1e3cce0f39691730ceb
Viktor Kindrat <84918090+viktor-kindrat@users.noreply.github.com>
2026-01-29 21:21:17 +0200

    Remove stub packages and restore npm dependencies

--- packages/nestjs-common/index.js
+++
@@ -1,8 +0,0 @@
-const decoratorFactory = () => (target, key, descriptor) => descriptor || target;
-
-module.exports = {
-  Injectable: decoratorFactory,
-  Module: decoratorFactory,
-  Controller: decoratorFactory,
-  Get: decoratorFactory,
-};
